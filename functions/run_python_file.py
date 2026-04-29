5a921fdec1e1e9255fb840f2432e0a52391b03ea
NickoGNS <nickgiannopbiz@gmail.com>
2026-04-29 11:55:10 +0300

    add run_python_file func

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,5 @@
 import os
-
+import subprocess
 
 def run_python_file(working_dir, file_path, args=None):
     try:
@@ -11,11 +11,26 @@ def run_python_file(working_dir, file_path, args=None):
             not os.path.commonpath([target_file_path, working_dir_abs])
             == working_dir_abs
         ):
-            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(target_file_path):
             return f'Error: "{file_path}" does not exist or is not a regular file'
         if not target_file_path.endswith(".py"):
             return f'Error: "{file_path}" is not a Python file'
 
+        command = ["python", target_file_path]
+
+        if args:
+            command.extend(args)
+
+        sp = subprocess.run(command, capture_output=True, text=True, timeout=30)
+
+        if sp.returncode != 0:
+            return f"Process exited with code {sp.returncode}"
+
+        if not sp.stdout and not sp.stderr:
+            return "No output produced"
+
+        return f"STDOUT: {sp.stdout}, STDERR: {sp.stderr}"
+
     except Exception as err:
-        return f"Error: {err}"
+        return f"Error: executing Python file: {err}"
