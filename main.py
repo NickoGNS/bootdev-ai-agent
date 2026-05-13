e5cb4bfd7f030234392630de212ae911b98ca2be
NickoGNS <nickgiannopbiz@gmail.com>
2026-05-13 14:19:50 +0300

    add agent prompt iterations, enhance instructions and finalize logic

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def generate_content(client, messages, verbose):
     for attempt in range(attempts):
         try:
             res = client.models.generate_content(
-                model="gemini-2.5-flash-lite",
+                model="gemini-2.5-flash",
                 contents=messages,
                 config=types.GenerateContentConfig(
                     tools=[available_functions], system_instruction=system_prompt
@@ -37,6 +37,7 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", res.usage_metadata.prompt_token_count)
         print("Response tokens:", res.usage_metadata.candidates_token_count)
 
+    final_text = None
     func_results = []
     if res.function_calls:
         for func in res.function_calls:
@@ -55,8 +56,12 @@ def generate_content(client, messages, verbose):
             if verbose:
                 print(f"-> {func_res.parts[0].function_response.response}")
     else:
-        print("Response:")
-        print(res.text)
+        final_text = res.text
+    return {
+        "candidates": res.candidates,
+        "function_results": func_results,
+        "final_response": final_text,
+    }
 
 
 load_dotenv()
@@ -68,17 +73,40 @@ if api_key is None or len(api_key) == 0:
     raise RuntimeError("Gemini api key wasn't found")
 
 def main():
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser(description="Chatbot")
+        parser.add_argument("user_prompt", type=str, help="User prompt")
+        parser.add_argument(
+            "--verbose", action="store_true", help="Enable verbose output"
+        )
+        args = parser.parse_args()
 
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}\n")
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}\n")
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+        ]
 
-    generate_content(gemini_client, messages, args.verbose)
+        final_response = None
+        for _ in range(20):
+            res = generate_content(gemini_client, messages, args.verbose)
+            for c in res["candidates"]:
+                messages.append(c.content)
+            for fr in res["function_results"]:
+                messages.append(types.Content(role="user", parts=[fr]))
+            if res["final_response"]:
+                final_response = res["final_response"]
+            if len(res["function_results"]) == 0:
+                break
+        if not final_response:
+            print("Agent iterations exceeded limit")
+            exit(1)
+        print("Final Response")
+        print(final_response)
+    except Exception as err:
+        print(err)
+        exit(1)
 
 
 if __name__ == "__main__":
